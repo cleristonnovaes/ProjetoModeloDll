7b8bfe8d527f0a7c236d4d015da41a65dde1a2e4
Cleriston <novaes.cleriston@gmail.com>
2021-09-30 15:29:10 -0300

    Packages NuGet

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
 
 namespace ByteBank.SistemaAgencia
 {
@@ -8,11 +10,18 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(123, 876456);
-            Funcionario funcionario = null;
+            DateTime datafimPagamento = new DateTime(2021, 10, 07);
 
-            Console.WriteLine(conta.Numero);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = datafimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
             Console.ReadLine();
         }
+       
     }
 }
